dc64765c866a236d1ddd2e4ea357f5c8a6964753
Charlie Franklin <charlie.franklin@me.com>
2014-11-15 15:12:16 +1100

    Struct created

---
+++ LionsAndTigers@/Tiger.swift
@@ -0,0 +1,13 @@
+//
+//  Tiger.swift
+//  LionsAndTigers@
+//
+//  Created by Charlie Franklin on 15/11/2014.
+//  Copyright (c) 2014 Charlie Franklin. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+    <#properties and methods#>
+}
